747daec1b5d5723ba9e36fa683454b6c2df44468
fannglu <fanglulim96@gmail.com>
2022-02-21 16:41:23 -0800

    fix code for all pages

--- js/activities.js
+++ js/activities.js
@@ -1,32 +1,21 @@
 $(document).ready(function () {
-    
-    $("td").click(function() {
-        var act = $(this).text();
-        if (act != "Not Available") {
-            $(this).toggleClass("tdHighlight");
-        }
+  $("td").click(function () {
+    var act = $(this).text();
+    if (act != "Not Available") {
+      $(this).toggleClass("tdHighlight");
+    }
 
-        if ($(this).hasClass("tdHighlight")) {
-        $('#displaySelected').css("visibility","visible");
-        $('#displaySelected').css("margin top" , "2em");
-        $('#result').append("<p>" + act + "</p>");
-    
-        } else {
-        $('#result p:contains('+act+')').remove();
+    if ($(this).hasClass("tdHighlight")) {
+      $("#displaySelected").css("visibility", "visible");
+      $("#displaySelected").css("margin top", "2em");
+      $("#result").append("<p>" + act + "</p>");
+    } else {
+      $("#result p:contains(" + act + ")").remove();
 
-        if($('#result').has('p').length == false) {
-            $('#displaySelected').css("visbility","hiden");
-            $('#displaySelected').css("margin-top","0");
-            
-
-        }
-    } 
-
-
-
-
-    });
-
-    
-
+      if ($("#result").has("p").length == false) {
+        $("#displaySelected").css("visbility", "hiden");
+        $("#displaySelected").css("margin-top", "0");
+      }
+    }
+  });
 });
