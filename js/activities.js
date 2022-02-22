b6af13913b357e3e8234acaba46470fc0295fe5a
fannglu <fanglulim96@gmail.com>
2022-02-22 15:55:44 -0800

    table help

--- js/activities.js
+++ js/activities.js
@@ -6,14 +6,16 @@ $(document).ready(function () {
     }
 
     if ($(this).hasClass("tdHighlight")) {
-      $("#displaySelected").css("visibility", "visible");
+      // $("#displaySelected").css("visibility", "visible"); //remove this for modal
       $("#displaySelected").css("margin top", "2em");
       $("#result").append("<p>" + act + "</p>");
+      $("#myModal").modal("show"); //added this for modal
     } else {
       $("#result p:contains(" + act + ")").remove();
+      $("#myModal").modal("show");   //added this for modal 
 
       if ($("#result").has("p").length == false) {
-        $("#displaySelected").css("visbility", "hiden");
+        // $("#displaySelected").css("visbility", "hiden"); //remove this for modal
         $("#displaySelected").css("margin-top", "0");
       }
     }
